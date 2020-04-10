97f3927546dc6b28417fd866abe0081c7463b338
tezzarida <tezzarida@gmail.com>
2020-04-10 16:54:35 +0200

    Updates

---
+++ precache-manifest.0636ce566f1017804c575385064fa7af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a8410c9b165f107815e9bcb2b39b4c",
+    "url": "/meme-generator-app/index.html"
+  },
+  {
+    "revision": "ceea3e85b3cdd0120f6b",
+    "url": "/meme-generator-app/static/css/main.6c1b98c1.chunk.css"
+  },
+  {
+    "revision": "93a458064038a1fda459",
+    "url": "/meme-generator-app/static/js/2.9b52927b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator-app/static/js/2.9b52927b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceea3e85b3cdd0120f6b",
+    "url": "/meme-generator-app/static/js/main.8c1412d0.chunk.js"
+  },
+  {
+    "revision": "331df71510c445791589",
+    "url": "/meme-generator-app/static/js/runtime-main.9d4f0bbb.js"
+  }
+]);
