c55241bed25e9a2ca99c54ebdc05a34d46bd3b56
tezzarida <tezzarida@gmail.com>
2020-04-11 15:23:29 +0200

    Add Header and MemeGenerator Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Header from './Header'
+import MemeGenerator from './MemeGenerator'
+
+/**
+ * Create 2 new components - Header and MemeGenerator
+ * Header will only display things
+ * MemeGenerator will be calling to an API and holding on to data
+ * Each should be in their own file of the same name
+ */
 
 function App() {
   return (
-    <h1>Hi there!</h1>
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+
   );
 }
 
